2e685496a9ce48a000c6f8414b1c138f357bb7c0
Nguyễn Hữu Thịnh <22130269@st.hcmuaf.edu.vn>
2025-08-13 15:00:31 +0700

    - Them @EnableFeignClients de co the ket noi voi profile-service

--- identity-service/src/main/java/com/dev/identity_service/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/dev/identity_service/IdentityServiceApplication.java
@@ -2,12 +2,14 @@ package com.dev.identity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // Enable Feign clients for service-to-service communication
 public class IdentityServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IdentityServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceApplication.class, args);
+    }
 
 }
