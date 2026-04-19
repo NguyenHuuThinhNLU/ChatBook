4e6d364cbe9f81cf6056d7088b8e09c2dce5bb9e
Nguyễn Hữu Thịnh <22130269@st.hcmuaf.edu.vn>
2026-04-19 22:56:55 +0700

    Thực hiện format time theo kiểu design pattern map/dictionary strategyMap

--- post-service/src/main/java/com/dev/post_service/service/PostService.java
+++ post-service/src/main/java/com/dev/post_service/service/PostService.java
@@ -26,6 +26,7 @@ import java.util.List;
 public class PostService {
     PostRepository postRepository;
     PostMapper postMapper;
+    DateTimeFormatter dateTimeFormatter;
 
 
     public PostResponse createPost(PostRequest postRequest) {
@@ -52,7 +53,11 @@ public class PostService {
         Sort sort = Sort.by("createdDate").descending();
         Pageable pageable = PageRequest.of(page - 1, size, sort);
         var pageData = postRepository.findAllByUserId(userId, pageable);
-
+        var postList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
 
 //        return postRepository.findAllByUserId(userId).stream().map(postMapper::toPostResponse).toList();
 
@@ -61,7 +66,7 @@ public class PostService {
                 .pageSize(pageData.getSize())
                 .totalPage(pageData.getTotalPages())
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .data(postList)
                 .build();
     }
 
